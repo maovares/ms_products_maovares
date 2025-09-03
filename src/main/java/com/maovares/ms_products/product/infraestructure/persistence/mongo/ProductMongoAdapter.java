78cb1020cff9338da567fccb792f68aa36e444e3
Marcos Rodríguez Ovares <mrodriguezovares@gmail.com>
2025-09-03 07:43:50 -0600

    sort by _id

--- src/main/java/com/maovares/ms_products/product/infraestructure/persistence/mongo/ProductMongoAdapter.java
+++ src/main/java/com/maovares/ms_products/product/infraestructure/persistence/mongo/ProductMongoAdapter.java
@@ -2,6 +2,7 @@ package com.maovares.ms_products.product.infraestructure.persistence.mongo;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
@@ -22,6 +23,7 @@ public class ProductMongoAdapter implements ProductRepository {
     public List<Product> findAll(int page, int size) {
         int skip = page * size;
         Query query = new Query().skip(skip).limit(size);
+        query.with(Sort.by(Sort.Direction.ASC, "_id"));
         List<ProductDocument> docs = mongoTemplate.find(query, ProductDocument.class);
         return docs.stream()
                 .map(doc -> new Product(doc.getId(), doc.getPrice(), doc.getDescription()))
