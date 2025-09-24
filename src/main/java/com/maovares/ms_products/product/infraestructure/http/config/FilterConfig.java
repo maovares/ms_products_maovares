ce2110213e8fb69141139f2310942243f0274201
Marcos Rodríguez Ovares <mrodriguezovares@gmail.com>
2025-09-24 09:23:14 -0600

    Filter Correlation handle by SpringBoot

--- src/main/java/com/maovares/ms_products/product/infraestructure/http/config/FilterConfig.java
+++ src/main/java/com/maovares/ms_products/product/infraestructure/http/config/FilterConfig.java
@@ -5,20 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.maovares.ms_products.product.infraestructure.http.filter.ClientCertValidationFilter;
-import com.maovares.ms_products.product.infraestructure.http.filter.CorrelationIdFilter;
 
 @Configuration
 public class FilterConfig {
-
-    @Bean
-    public FilterRegistrationBean<CorrelationIdFilter> correlationIdFilter() {
-        FilterRegistrationBean<CorrelationIdFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new CorrelationIdFilter());
-        registrationBean.addUrlPatterns("/*");
-        registrationBean.setOrder(1);
-        registrationBean.setName("correlationIdFilter");
-        return registrationBean;
-    }
 
     @Bean
     public FilterRegistrationBean<ClientCertValidationFilter> clientCertValidationFilter() {
